af8a8bb12e28b44be8a76ec4621225f7364ae3a2
Sa-Hesham <elsayedheshamdarwish@gmail.com>
2025-07-06 17:53:22 +0300

    one Diamation Array

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,7 +4,57 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region 1D-Array
+
+            //array is adata structure 
+            // array is a single varible allow  to store multiuble data  that have the same type 
+            // array [class referance type ]
+            // 1- fixed size 
+            //2- zero based indexing 
+            //3- same data type 
+
+            // 1 dimantioal decleration 
+
+            //int[] arr = new int[10];   
+            //ClR create refernace in stack [4-8 byte ]
+            // that referance to adress  in anoter object in heap 
+            // CLR create object at heap contguesly has same data type with 40 byte 
+            // feferane data type has adress of index zero
+
+
+            //int[]arr02=new int[5] {1,2,3,4,5};
+
+            //int[] arr03=new int[] {1,2,3,4,5};
+
+            //int[] arr04 = { 1, 2, 3, 4, 5 };
+
+            //int[] arr05 = [1,2,3,4,5];
+
+
+            // advanges of array : 
+            // access time => can arrive to any element by one step  [byte * index + adress ]  (o(1));
+
+            //disadvages ==> fixed Size
+
+
+            // retreving data 
+
+            //for (int i = 0; i < arr04.Length; i++) {
+
+            //    Console.WriteLine(arr04[i]);
+            
+            
+            //}
+
+
+            #endregion
+
+
+
+
+
+
         }
     }
 }
