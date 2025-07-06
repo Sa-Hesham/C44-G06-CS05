abbc134984c449c43f9d6c66b87951ce2ae3040d
Sa-Hesham <elsayedheshamdarwish@gmail.com>
2025-07-07 01:04:07 +0300

    Question1

--- assignment/Program.cs
+++ assignment/Program.cs
@@ -1,10 +1,54 @@
-﻿namespace assignment
+﻿using System.Drawing;
+
+namespace assignment
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+            #region Q1
+
+
+            //1 - .Write a program that prints an identity matrix using for loop, in other
+            //words takes a value n from the user and shows the identity table of size n * n.
+            //1 0 0
+            //0 1 0
+            // 0 0 1       main diagonal have the same values 
+
+
+
+            //Console.WriteLine($"please enter the matrix size ::");
+            //int.TryParse(Console.ReadLine(), out int N);
+
+
+            //Console.Clear();
+
+            //for (int i = 0; i < N; i++)
+            //{
+
+            //    for (int j = 0; j < N; j++)
+            //    {
+
+            //        if (i == j)
+            //        {
+            //            Console.Write("1 \t");
+            //        }
+            //        else
+            //        {
+            //            Console.Write("0 \t");
+            //        }
+
+
+
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+
         }
     }
 }
